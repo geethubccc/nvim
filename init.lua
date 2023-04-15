b974e968f3573fa6d0fc886e228bd80efa6c8606
c <kknyna001@qq.com>
2023-04-15 20:15:23 +0800

    My nvim configuration

---
+++ init.lua
@@ -0,0 +1,21 @@
+require("ccc.plugins-setup")
+
+require("ccc.core.options")
+require("ccc.core.keymaps")
+
+require("ccc.core.colorscheme")
+
+-- plugins
+require("ccc.plugins.comment")
+require("ccc.plugins.nvim-tree")
+require("ccc.plugins.lualine")
+require("ccc.plugins.telescope")
+require("ccc.plugins.nvim-cmp")
+require("ccc.plugins.lsp.mason")
+require("ccc.plugins.lsp.lspconfig")
+require("ccc.plugins.lsp.null-ls")
+require("ccc.plugins.lsp.trouble")
+require("ccc.plugins.autopairs")
+require("ccc.plugins.treesitter")
+require("ccc.plugins.gitsigns")
+require("ccc.plugins.bufferline")
